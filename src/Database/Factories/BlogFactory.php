43f8d318fe5bf7af65aec8070600364a8eeb233f
Seth Sharp <sesharp@outlook.com>
2024-04-01 13:49:50 +1000

    remove config in factory

--- src/Database/Factories/BlogFactory.php
+++ src/Database/Factories/BlogFactory.php
@@ -19,7 +19,7 @@ class BlogFactory extends Factory
 
         return [
             'author_id' => User::factory()->create()->id,
-            'cover_image' => config('app.cloudfront_url') . $this->getRandomCover(),
+            'cover_image' => $this->getRandomCover(),
             'is_draft' => false,
             'title' => $name,
             'slug' => $slug,
