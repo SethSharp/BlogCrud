99cf17de2a014e9ac27772b3f564e982155026d8
Seth Sharp <sesharp@outlook.com>
2024-06-14 11:43:21 +1000

    migrations and updates to relatinships

---
+++ database/migrations/2024_06_14_012828_add_blog_id_to_comments_table.php
@@ -0,0 +1,18 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('comments', function (Blueprint $table) {
+            $table->foreignId('blog_id')->after('user_id');
+        });
+    }
+};
--- src/Models/Blog/Comment.php
+++ src/Models/Blog/Comment.php
@@ -3,10 +3,9 @@
 namespace SethSharp\BlogCrud\Models\Blog;
 
 use Illuminate\Database\Eloquent\Model;
-use Illuminate\Database\Eloquent\Relations\HasOne;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use SethSharp\BlogCrud\Models\Events\CommentCreatedEvent;
-use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 use SethSharp\BlogCrud\Database\Factories\Domain\Blog\Models\CommentFactory;
 
 class Comment extends Model
@@ -24,13 +23,13 @@ class Comment extends Model
         return new CommentFactory();
     }
 
-    public function blogs(): BelongsToMany
+    public function blog(): BelongsTo
     {
-        return $this->belongsToMany(config('blog-crud.models.blog.blog'), 'blog_comment', 'comment_id', 'blog_id');
+        return $this->belongsTo(config('blog-crud.models.blog.blog'));
     }
 
-    public function user(): HasOne
+    public function user(): BelongsTo
     {
-        return $this->hasOne(config('blog-crud.models.iam.user'), 'id', 'user_id');
+        return $this->belongsTo(config('blog-crud.models.iam.user'));
     }
 }
