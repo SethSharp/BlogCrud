fdcad41e80353b400a1ac394a499207f2c863bd6
Seth Sharp <sesharp@outlook.com>
2024-06-18 12:41:32 +1000

    Revert "add missing Like relationships to User and Blog"
    This reverts commit 97c4f1aafbe5c4d28969b8a6c148fd045a0195af.

--- src/Models/Blog/Like.php
+++ src/Models/Blog/Like.php
@@ -3,7 +3,6 @@
 namespace SethSharp\BlogCrud\Models\Blog;
 
 use Illuminate\Database\Eloquent\Model;
-use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use SethSharp\BlogCrud\Models\Events\LikeCreatedEvent;
 
 class Like extends Model
@@ -13,14 +12,4 @@ class Like extends Model
     protected $dispatchesEvents = [
         'created' => LikeCreatedEvent::class
     ];
-
-    public function blog(): BelongsTo
-    {
-        return $this->belongsTo(config('blog-crud.models.blog.blog'));
-    }
-
-    public function user(): BelongsTo
-    {
-        return $this->belongsTo(config('blog-crud.models.iam.user'));
-    }
 }
